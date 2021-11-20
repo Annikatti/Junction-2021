b4c7e5506114d2705ddf987e9e0234af37940302
Annikatti <annika.tikki@hotmail.com>
2021-11-20 21:21:35 +0200

    first again

---
+++ index.js
@@ -0,0 +1,25 @@
+const activeFilters = [];
+
+function addWord() {
+  const filterBy = document.getElementById("filter-by");
+  activeFilters.push(filterBy.value.trim());
+  filterBy.value = "";
+
+  document.getElementById("active-filters").innerHTML = activeFilters;
+}
+
+function setFilter(filterOn) {
+  document.getElementById("filtered-words").style.color = filterOn ? 'black' : 'grey';
+}
+
+function changeTab(newTab) {
+  ["play", "store", "settings"].forEach(tab => {
+    if (newTab === tab) {
+      document.getElementById(tab).classList.add("display");
+      document.getElementById(`${tab}-button`).classList.add("active");
+    } else {
+      document.getElementById(tab).classList.remove("display");
+      document.getElementById(`${tab}-button`).classList.remove("active");
+    }
+  })
+}
