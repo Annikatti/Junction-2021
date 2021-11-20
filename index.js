61dc9d4e1d018a67277a80e2f98ac715feec1abc
Risto Kyötikki <risto.kyotikki@outlook.com>
2021-11-20 21:41:53 +0200

    require username for sending message. use postMessage

--- index.js
+++ index.js
@@ -1,90 +1,100 @@
 const activeFilters = [];
-const username = 'riesa';
+const username = null;
 let latestMessages = null;
 
 function addWord() {
-  const filterBy = document.getElementById("filter-by");
-  activeFilters.push(filterBy.value.trim());
-  filterBy.value = "";
+    const filterBy = document.getElementById("filter-by");
+    activeFilters.push(filterBy.value.trim());
+    filterBy.value = "";
 
-  document.getElementById("active-filters").innerHTML = activeFilters;
+    document.getElementById("active-filters").innerHTML = activeFilters;
 }
 
 function setFilter(filterOn) {
-  document.getElementById("filtered-words").style.color = filterOn ? 'black' : 'grey';
+    document.getElementById("filtered-words").style.color = filterOn ? 'black' : 'grey';
 }
 
 function changeTab(newTab) {
-  ["play", "store", "settings"].forEach(tab => {
-    if (newTab === tab) {
-      document.getElementById(tab).classList.add("display");
-      document.getElementById(`${tab}-button`).classList.add("active");
-    } else {
-      document.getElementById(tab).classList.remove("display");
-      document.getElementById(`${tab}-button`).classList.remove("active");
-    }
-  })
+    ["play", "store", "settings"].forEach(tab => {
+        if (newTab === tab) {
+            document.getElementById(tab).classList.add("display");
+            document.getElementById(`${tab}-button`).classList.add("active");
+        } else {
+            document.getElementById(tab).classList.remove("display");
+            document.getElementById(`${tab}-button`).classList.remove("active");
+        }
+    })
 }
 
 function getLatestMessages() {
-  const xmlHttp = new XMLHttpRequest();
+    const xmlHttp = new XMLHttpRequest();
 
-  try {
-    xmlHttp.open( "GET", 'https://us-central1-junction-2021-fee21.cloudfunctions.net/webApi/api/v1/messages', false );
-    xmlHttp.send( null );
-    latestMessages = JSON.parse(xmlHttp.responseText).sort((a,b) => a.data.createdAt > b.data.createdAt);
+    try {
+        xmlHttp.open("GET", 'https://us-central1-junction-2021-fee21.cloudfunctions.net/webApi/api/v1/messages', false);
+        xmlHttp.send(null);
+        latestMessages = JSON.parse(xmlHttp.responseText).sort((a, b) => a.data.createdAt > b.data.createdAt);
 
-    const chatWindow = document.getElementById("chat");
-    chatWindow.innerHTML = "";
-    latestMessages.forEach(message => {
-      const messageContainer = document.createElement("div");
-      messageContainer.setAttribute("class", "message-container");
-      messageContainer.setAttribute("id", message.id);
-      
-      const messageContent = document.createElement("p");
-      messageContent.innerHTML = `${message.data.username}: ${message.data.message}`;
-      messageContent.setAttribute("class", "message-content");
-      
-      const messageCreatedAt = document.createElement("p");
-      messageCreatedAt.innerHTML = message.data.createdAt;
-      messageCreatedAt.setAttribute("class", "message-created-at");
+        const chatWindow = document.getElementById("chat");
+        chatWindow.innerHTML = "";
+        latestMessages.forEach(message => {
+            const messageContainer = document.createElement("div");
+            messageContainer.setAttribute("class", "message-container");
+            messageContainer.setAttribute("id", message.id);
 
-      messageContainer.appendChild(messageContent);
-      messageContainer.appendChild(messageCreatedAt);
-      chatWindow.appendChild(messageContainer);
-    })
-  } catch (error) {
-    console.log(error);
-  }
+            const messageContent = document.createElement("p");
+            messageContent.innerHTML = `${message.data.username}: ${message.data.message}`;
+            messageContent.setAttribute("class", "message-content");
+
+            const messageCreatedAt = document.createElement("p");
+            messageCreatedAt.innerHTML = message.data.createdAt;
+            messageCreatedAt.setAttribute("class", "message-created-at");
+
+            messageContainer.appendChild(messageContent);
+            messageContainer.appendChild(messageCreatedAt);
+            chatWindow.appendChild(messageContainer);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function changeUsername() {
+    username = document.getElementById("username").value;
 }
 
 function emphatizeMessage(messageContent) {
-  const xmlHttp = new XMLHttpRequest();
+    const xmlHttp = new XMLHttpRequest();
 
-  try {
-    xmlHttp.open( "POST", '', true );
-    xmlHttp.setRequestHeader('Content-Type', 'application/json');
-    xmlHttp.send( JSON.stringify({"message": messageContent}) );
-    xmlHttp.onload = () => postMessage(xmlHttp.responseText);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        xmlHttp.open("POST", '', true);
+        xmlHttp.setRequestHeader('Content-Type', 'application/json');
+        xmlHttp.send(JSON.stringify({ "message": messageContent }));
+        xmlHttp.onload = () => postMessage(xmlHttp.responseText);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 function postMessage(messageContent) {
-  const xmlHttp = new XMLHttpRequest();
-  try {
-    xmlHttp.open( "POST", 'https://us-central1-junction-2021-fee21.cloudfunctions.net/webApi/api/v1/message', true );
-    xmlHttp.setRequestHeader('Content-Type', 'application/json');
-    xmlHttp.send( JSON.stringify({"username": username, "message": messageContent}) );
-    xmlHttp.onload = () => getLatestMessages();
-  } catch (error) {
-    console.log(error);
-  }
+    const xmlHttp = new XMLHttpRequest();
+    try {
+        xmlHttp.open("POST", 'https://us-central1-junction-2021-fee21.cloudfunctions.net/webApi/api/v1/message', true);
+        xmlHttp.setRequestHeader('Content-Type', 'application/json');
+        xmlHttp.send(JSON.stringify({ "username": username, "message": messageContent }));
+        xmlHttp.onload = () => getLatestMessages();
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function sendMessage() {
-  const messageElement = document.getElementById("message");
-  emphatizeMessage(messageElement.value);
-  messageElement.value = "";
+    if (!username) {
+        alert("You need to give a username!");
+        return;
+    }
+
+    const messageElement = document.getElementById("message");
+    // emphatizeMessage(messageElement.value);
+    postMessage(messageElement.value);
+    messageElement.value = "";
 }
